f4d7f1c207afd765f65e573e4041891a34365358
mamello18 <mamellomampe55@gmail.com>
2025-12-10 14:30:09 +0200

    Remove backup folder

--- oop/book_class.py
+++
@@ -1,8 +0,0 @@
-class Book:
-    def __init__(self, title, author, year):
-        self.title = title
-        self.author = author
-        self.year = year
-
-    def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.year}"
