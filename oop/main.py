aa5dc1d5cc48f8629c024082db4d29a943f6fd01
mamello18 <mamellomampe55@gmail.com>
2025-12-11 16:33:54 +0200

    Add polymorphism demo project

--- oop/main.py
+++ oop/main.py
@@ -1,14 +1,15 @@
-from library_system import Book, EBook, PrintBook, Library
+from polymorphism_demo import Shape, Rectangle, Circle
+import math
 
-library = Library()
+def main():
+    shapes = [
+        Rectangle(10, 5),
+        Circle(7)
+    ]
 
-book1 = Book("Pride and Prejudice", "Jane Austen")
-ebook1 = EBook("Snow Crash", "Neal Stephenson", "500KB")
-printbook1 = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
 
-library.add_book(book1)
-library.add_book(ebook1)
-library.add_book(printbook1)
-
-library.list_books()
+if __name__ == "__main__":
+    main()
 
